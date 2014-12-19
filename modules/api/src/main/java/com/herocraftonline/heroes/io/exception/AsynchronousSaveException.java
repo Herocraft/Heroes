5a38e8f8694ae66a7c529563f7dcd5a382901f6e
Andrew2060 <2060andrew@gmail.com>
2014-12-18 17:10:04 -0800

    Document what AsyncSaveException does

--- modules/api/src/main/java/com/herocraftonline/heroes/io/exception/AsynchronousSaveException.java
+++ modules/api/src/main/java/com/herocraftonline/heroes/io/exception/AsynchronousSaveException.java
@@ -2,7 +2,11 @@ package com.herocraftonline.heroes.io.exception;
 
 import com.herocraftonline.heroes.characters.Hero;
 
-public class AsynchronousSaveException extends RuntimeException {
+/**
+ * Thrown when asynchronous saving fails, for whatever reason. If the failure was due to an exception,
+ * said exception is included as the cause.
+ */
+public final class AsynchronousSaveException extends RuntimeException {
     public AsynchronousSaveException(Hero hero, Exception cause) {
         super("A problem arose during asynchronous save of " + hero.getName(), cause);
     }
