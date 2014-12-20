1e7409f9f12478b4dcb739e446bad6ed2087fbfd
Andrew2060 <2060andrew@gmail.com>
2014-12-20 14:05:47 -0800

    Missed this file in last commit

---
+++ modules/common/src/main/java/com/herocraftonline/heroes/common/effects/TickableEffect.java
@@ -0,0 +1,40 @@
+package com.herocraftonline.heroes.common.effects;
+
+import com.herocraftonline.heroes.characters.CharacterBase;
+import com.herocraftonline.heroes.effects.EffectTickable;
+import com.herocraftonline.heroes.plugin.HeroesPlugin;
+
+public abstract class TickableEffect extends BasicEffect implements EffectTickable {
+    protected long nextTick;
+    protected boolean removed;
+
+    public TickableEffect(HeroesPlugin plugin, String name) {
+        super(plugin, name);
+        removed = false;
+    }
+
+    @Override
+    public void apply(CharacterBase character) {
+        nextTick = System.currentTimeMillis();
+    }
+
+    @Override
+    public void remove(CharacterBase character) {
+        removed = true;
+    }
+
+    @Override
+    public boolean canTick(long currTime) {
+        return currTime >= nextTick;
+    }
+
+    @Override
+    public boolean shouldStopTicking(long currTime) {
+        return removed;
+    }
+
+    @Override
+    public void tick(CharacterBase character) {
+        nextTick += timeTillNextTick();
+    }
+}
