a8f024d095c91360ae09702477ffae213a784054
Andrew2060 <2060andrew@gmail.com>
2014-12-22 03:02:42 -0800

    Remove redundant EffectRepeatable interface

--- modules/api/src/main/java/com/herocraftonline/heroes/effects/EffectPeriodic.java
+++ modules/api/src/main/java/com/herocraftonline/heroes/effects/EffectPeriodic.java
@@ -4,7 +4,7 @@ package com.herocraftonline.heroes.effects;
  * Periodic implementations indicate that the effect is periodic in nature - that is to say its effects repeat on some
  * constant period ({@link #timeTillNextTick()} will return a constant value).
  */
-public interface EffectPeriodic extends EffectRepeatable {
+public interface EffectPeriodic extends EffectTickable {
     /**
      * @return The period between ticks in milliseconds, same value as {@link #timeTillNextTick()}
      */
