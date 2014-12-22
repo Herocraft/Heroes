e8637044981c33fd1e40adeeec4252d6c2233f77
Andrew2060 <2060andrew@gmail.com>
2014-12-21 22:35:52 -0800

    CharacterBase not Character

--- modules/api/src/main/java/com/herocraftonline/heroes/classes/CharacterClass.java
+++ modules/api/src/main/java/com/herocraftonline/heroes/classes/CharacterClass.java
@@ -1,5 +1,7 @@
 package com.herocraftonline.heroes.classes;
 
+import com.herocraftonline.heroes.characters.CharacterBase;
+
 /**
  * Represents a class - classes store component settings and will automatically apply relevant components with
  * initial settings when it is attached. In addition classes have levels and experience sources.
@@ -7,28 +9,29 @@ package com.herocraftonline.heroes.classes;
  */
 public interface CharacterClass {
 
+
     //Experience related methods
 
     /**
      * @param character The character to get experience for
      * @return A numerical representation of the number of experience points accumulated by the character
-     * @see #getLevelExperience(Character)
+     * @see #getLevelExperience(CharacterBase)
      */
-    long getExperience(Character character);
+    long getExperience(CharacterBase character);
 
     /**
      * @param character The character to get experience for
      * @return The amount of experience the character needs to reach the next level ({@link #getLevel(Character)} + 1)
      *         with 0 exp.
      */
-    long getExperienceToNextLevel(Character character);
+    long getExperienceToNextLevel(CharacterBase character);
 
     /**
      * @param character The character to get experience for
      * @return The amount of experience the character has accumulated in its current level
      * @see #getExperience(Character)
      */
-    long getLevelExperience(Character character);
+    long getLevelExperience(CharacterBase character);
 
     /**
      * @param character The character to check the level of
@@ -51,13 +54,15 @@ public interface CharacterClass {
      */
     String getName();
 
+    boolean isMastered(CharacterBase character);
+
     /**
      * Sets the amount of experience a given character has in this class
      * @param character The character to set the experience of
      * @param experience The experience amount, valid range being [0, {@link #getMaxExperience()}]
      * @throws java.lang.IllegalArgumentException if {@code experience < 0} or {@code experience > getMaxExperience()}
      */
-    void setExperience(Character character, long experience);
+    void setExperience(CharacterBase character, long experience);
 
     /**
      * Sets the level (with 0 exp towards the next level) a given character has in this class
@@ -65,7 +70,7 @@ public interface CharacterClass {
      * @param level The level to set, valid range being [0, {@link #getMaxLevel()}]
      * @throws java.lang.IllegalArgumentException if {@code level < 0} or {@code level > getMaxLevel()}
      */
-    void setLevel(Character character, int level);
+    void setLevel(CharacterBase character, int level);
 
     //Component Methods
 
